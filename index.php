d23364d8bafd2487288cbfb988e60e9bbd130aa6
jensvanlankveld <jensvanlankveld@Live.nl>
2015-05-27 12:14:04 +0200

    index update

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-dit is
-bestand
+<?php
+require('configuratie.php');
 
-ssss
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Temp</title>
+		<link href="" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<header>
+			<?php
+			require('./modules/header.php');
+			require('./modules/menu.php');
+			?>
