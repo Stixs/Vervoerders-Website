80981cec1362e9697ba0d046552cfa4f9f949255
jensvanlankveld <jensvanlankveld@Live.nl>
2015-05-27 12:42:32 +0200

    switch statment

--- src/index.php
+++ src/index.php
@@ -9,4 +9,30 @@ require('./modules/menu.php');
 
 require('./modules/footer.php')
 
+if(isset($_GET['paginanr']))
+{
+	$paginanr = $_GET['paginanr'];
+}
+else
+{
+	$paginanr = 0;
+}
+switch($paginanr)
+{
+	case 0:
+	require('./modules/home.php');
+	break;
+	case 1:
+	require('./modules/zoeken.php');
+	break;
+	case 2:
+	require('./modules/gids.php');
+	break;
+	case 50:
+	require('./modules/inloggen.php');
+	break;
+	case 51:
+	require('./modules/uitloggen.php');
+	break;
+}
 ?>
