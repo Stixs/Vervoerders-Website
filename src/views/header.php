0cd36c4b4b07c1d9dc6cca7c30a5ff4894f24fe6
Stixs <timwestland.94@gmail.com>
2015-05-27 13:24:46 +0200

    update

--- src/views/header.php
+++ src/views/header.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Vervoeders Website</title>
-		<link href="./style/style.php" type="text/css" rel="stylesheet" />
+		<link href="./style/stylej.php" type="text/css" rel="stylesheet" />
+		<link href="./style/stylet.php" type="text/css" rel="stylesheet" />
 	</head>
 	<body>
+		
