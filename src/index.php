aae17f89c42ab3705a198b2813c041e92f8e3750
Stixs <timwestland.94@gmail.com>
2015-05-27 12:33:28 +0200

    index update

--- src/index.php
+++ src/index.php
@@ -2,17 +2,9 @@
 require('configuratie.php');
 
 
+require('./modules/header.php');		
+require('./modules/menu.php');
+require('./modules/footer.php')
+
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Temp</title>
-		<link href="" type="text/css" rel="stylesheet" />
-	</head>
-	<body>
-		<header>
-			<?php
-			require('./modules/header.php');
-			require('./modules/menu.php');
-			?>
+			
---
+++ src/modules/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Temp</title>
+		<link href="" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
