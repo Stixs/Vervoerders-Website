e7f091f5be770842ea211f5f1064521798635d70
jensvanlankveld <jensvanlankveld@Live.nl>
2015-05-30 09:59:39 +0200

    update gids

--- src/controllers/gids.php
+++ src/controllers/gids.php
@@ -1,11 +1,11 @@
 <div class="bedrijfsp">
+	<span class="naam">(bedrijfs naam)</span>
 	<div class="info">
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis tortor ac consequat tristique. Nunc ex lacus, cursus at purus in, faucibus maximus justo. Quisque ac leo eget sapien feugiat lobortis. Mauris sed rutrum leo. Proin lobortis aliquet mollis. Suspendisse eu consequat lacus. Morbi vel pharetra justo. Fusce tincidunt id lacus quis posuere. In scelerisque arcu sit amet sem tempor tincidunt. Donec aliquet felis sit amet velit faucibus, vel ullamcorper risus efficitur. Integer viverra pretium ante, laoreet congue leo posuere ac. In arcu metus, varius id tellus nec, elementum sodales ligula. Etiam vulputate velit eget quam egestas auctor. Vivamus nibh nunc, pulvinar at eros a, venenatis dignissim quam. Praesent sagittis lacus ut arcu suscipit tincidunt.<br>
 		<br />
 		Curabitur ut ex nulla. Nunc aliquet lacus vitae blandit hendrerit. Fusce pretium aliquet elit sit amet viverra. Nulla ac metus consectetur, congue massa eu, mollis enim. Proin id libero ut leo gravida dictum id a erat. Praesent eu fringilla ante. Ut porttitor fringilla mi. Nunc tempus pretium nibh, ac accumsan mauris maximus in. Morbi at purus placerat, egestas augue sed, elementum ex. Mauris molestie egestas luctus.
 	</div>
 	<table class="contact">
-		<th colspan="2">(Naam bedrijf)</th>
 		<tr>
 			<td>Land:</td>
 			<td>(land)</td>
@@ -33,10 +33,6 @@
 		<tr>
 			<td>E-mail:</td>
 			<td>(email)</td>
-		</tr>
-		<tr>
-			<td>Skype-account:</td>
-			<td>(skype)</td>
 		</tr>
 	</table>
 </div>
