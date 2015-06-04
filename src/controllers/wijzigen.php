a6398f1bb237c1e553e63a69fff93c3eef63e938
jensvanlankveld <jensvanlankveld@Live.nl>
2015-06-04 15:36:59 +0200

    update wijzigen

--- src/controllers/wijzigen.php
+++ src/controllers/wijzigen.php
@@ -1,14 +1,40 @@
 <?php
+	if(isset($_POST['Wijzigenbedrijf']))
+{
+	$CheckOnErrors = false;
 	
+	$gebruikersnaam = $_POST["gebruikersnaam"];
+	$wachtwoord = $_POST["wachtwoord"];
+	$email = $_POST["email"];
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	if($CheckOnErrors == true) 
+	{
 	require('./views/WijzigenBedrijfForm.php');
+	}
+	else
+	{
+		//formulier is succesvol gevalideerd
+
+		//maak unieke salt
+		$salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
+
+		//hash het paswoord met de Salt
+		$wachtwoord = hash('sha512', $wachtwoord . $salt);
+
+		
+		$parameters = array(':gebruikersnaam'=>$gebruikersnaam,
+							':email'=>$email,
+							':wachtwoord'=>$wachtwoord,
+							':salt'=>$salt,
+							':level'=>5
+							);
+		$sth = $pdo->prepare('INSERT INTO gebruikers (gebruikersnaam, email, wachtwoord, salt, level) VALUES (:gebruikersnaam, :email, :wachtwoord, :salt, :level)');
+		$sth->execute($parameters);
+		echo 'test';
+	}
+}
+else
+{
+	require('./views/WijzigenBedrijfForm.php');
+}
 ?>
