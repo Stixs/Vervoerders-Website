e9358376be785325e4ea23418159e53ac6fbc6e5
jensvanlankveld <jensvanlankveld@Live.nl>
2015-06-11 10:09:52 +0200

    update wijzigen

--- src-back/views/WijzigenBedrijfForm.php
+++ src-back/views/WijzigenBedrijfForm.php
@@ -15,7 +15,7 @@
 		<td><label for="Plaats">Plaats:</label></td><td><input type="text" id="Plaats" name="plaats" value="<?php echo $plaats; ?>"  size="30"/></td>
 		</tr>
 		<tr>		
-		<td><label for="Land">Land:</label></td><td><input type="text" id="Land" name="land" value="<?php echo $land; ?>"  size="30"/></td>
+		<td><label for="provincies">provincies:</label></td><td><input type="text" id="provincies" name="provincies" value="<?php echo $provincies; ?>"  size="30"/></td>
 		</tr>
 		<tr>		
 		<td><label for="Telefoon">Telefoon:</label></td><td><input type="text" id="Telefoon" name="telefoon" value="<?php echo $telefoon; ?>"  size="30"/></td>
@@ -24,26 +24,31 @@
 		<td><label for="Fax">Fax:</label></td><td><input type="text" id="Fax" name="fax" value="<?php echo $fax; ?>"  size="30"/></td>
 		</tr>
 		<tr>		
-		<td><label for="Type">Type:</label></td><td><input type="text" id="Type" name="type" value="<?php echo $type; ?>"  size="30"/></td>
+		<td><label for="specialiteit">Specialiteit:</label></td><td><input type="text" id="specialiteit" name="specialiteit" value="<?php echo $specialiteit; ?>"  size="30"/></td>
 		</tr>
 		<tr>		
-		<td><label for="Naar">Naar:</label></td><td><input type="text" id="Naar" name="naar" value="<?php echo $naar; ?>"  size="30"/></td>
+		<td><label for="type">Type:</label></td><td><input type="text" id="type" name="type" value="<?php echo $type; ?>"  size="30"/></td>
 		</tr>
 		<tr>		
-		<td><label for="Specialiteit">Specialiteit:</label></td><td><input type="text" id="Specialiteit" name="specialiteit" value="<?php echo $specialiteit; ?>"  size="30"/></td>
+		<td><label for="bereik">Bereik:</label></td><td><input type="text" id="bereik" name="bereik" value="<?php echo $bereik; ?>"  size="30"/></td>
 		</tr>
 		<tr>
-		<td><label for="Email">Bedrijfs E-mail:</label></td><td><input type="text" id="Email" name="bedrijf_email" value="<?php echo $bedrijf_email; ?>"  size="30"/></td><td>
+		<td><label for="transportmanager">Transport-manager:</label></td><td><input type="text" id="transportmanager" name="transportmanager" value="<?php echo $transportmanager; ?>"  size="30"/></td>
 		</tr>
 		<tr>
-		<td><label for="Gebruikersnaam">Gebruikersnaam:</label></td><td><input type="text" id="Gebruikersnaam" name="gebruikersnaam" value="<?php echo $gebruikersnaam; ?>"  size="30"/></td>
+		<td><label for="aantal">Aantal:</label></td><td><input type="text" id="aantal" name="aantal" value="<?php echo $aantal; ?>"  size="30"/></td><td>
 		</tr>
 		<tr>
-		<td><label for="Wachtwoord">Wachtwoord:</label></td><td><input type="text" id="Wachtwoord" name="wachtwoord" value="<?php echo $wachtwoord; ?>"  size="30"/></td>
+		<td><label for="rechtsvorm">Rechtsvorm:</label></td><td><input type="text" id="rechtsvorm" name="rechtsvorm" value="<?php echo $rechtsvorm; ?>"  size="30"/></td>
 		</tr>
 		<tr>
-		<td><label for="Email">E-mail:</label></td><td><input type="text" id="Email" name="email" value="<?php echo $email; ?>" size="30"/></td><td>
+		<td><label for="vergunning">Vergunning:</label></td><td><input type="text" id="vergunning" name="vergunning" value="<?php echo $vergunning; ?>" size="30"/></td><td>
 		</tr>
+		<tr>
+		<td><label for="geldigtot">Geldig tot:</label></td><td><input type="text" id="geldigtot" name="geldigtot" value="<?php echo $geldigtot; ?>" size="30"/></td>
+		</tr>
+		<tr>
+		<td><label for="Email">Bedrijfs E-mail:</label></td><td><input type="text" id="bedrijf_email" name="bedrijf_email" value="<?php echo $bedrijf_email ?>" size="30"/></td>
 		<tr>		
 		<td><input type="submit" name="Wijzigenbedrijf" value="Wijzigen!" /></td>
 		</tr>
