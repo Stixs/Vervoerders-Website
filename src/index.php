067501b984997b6a7da939b26fa8b6c6ff63afe6
jensvanlankveld <jensvanlankveld@Live.nl>
2015-06-11 10:12:49 +0200

    update

--- src/index.php
+++ src/index.php
@@ -30,6 +30,9 @@ switch($paginanr)
 	case 3:
 	require('./controllers/zoeken.php');
 	break;
+	case 49:
+	require('./controllers/wijzigen.php');
+	break;
 	case 50:
 	require('./controllers/inloggen.php');
 	break;
