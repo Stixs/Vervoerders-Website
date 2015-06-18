6f74a9313981c632143055104756d73f59361421
jensvanlankveld <jensvanlankveld@Live.nl>
2015-06-18 09:03:16 +0200

    update wijzigen

--- src/controllers/wijzigen.php
+++ src/controllers/wijzigen.php
@@ -1,4 +1,10 @@
 <?php
+//init fields
+$bedrijfsnaam = $beschrijving = $adres = $postcode = $plaats = $provincie = $telefoon = $fax = $bedrijfs_email = $specialiteit = $type = $bereik = $transport_manager = $aantal = $rechtsvorm = $vergunning = $geldig_tot = NULL;
+
+//init error fields
+$NameErr = $AdresErr = $ZipErr = $CityErr = $TelErr = $FaxErr = $MailErr = NULL;
+
 
 	$userid = $_SESSION['user_id'];
 	
@@ -9,17 +15,36 @@
 	
 	$bedrijfs_id = $row['bedrijfs_id'];
 	
+	
 
 	$parameters = array(':bedrijfs_id'=>$bedrijfs_id);
 	$sth = $pdo->prepare('select * from bedrijfgegevens where bedrijfs_id = :bedrijfs_id');
 	$sth->execute($parameters);
 	$row = $sth->fetch();
 	
+	$bedrijf_naam = $row['bedrijfsnaam'];
+	$beschrijving = $row['beschrijving'];
+	$adres = $row['adres'];
+	$postcode = $row['postcode'];
+	$plaats = $row['plaats'];
+	$provincie = $row['provincie'];
+	$telefoon = $row['telefoon'];
+	$fax = $row['fax'];
+	$specialiteit = $row['specialiteit'];
+	$type = $row['type'];
+	$bereik = $row['bereik'];
+	$transport_manager = $row['transport_manager'];
+	$aantal = $row['aanta;'];
+	$rechtsvorm = $row['rechtsvorm'];
+	$vergunning = $row['vergunning'];
+	$geldigtot = $row['geldig_tot'];
+	$bedrijfs_email = $row['bedrijfs_email'];
+	
 	echo $row['adres'];
 
 	
 	
-/*	if(isset($_POST['Wijzigenbedrijf']))
+	if(isset($_POST['Wijzigenbedrijf']))
 {
 	$CheckOnErrors = false;
 	
@@ -40,5 +65,5 @@ else
 {
 	require('./views/WijzigenBedrijfForm.php');
 }
-*/
+
 ?>
--- src/views/WijzigenBedrijfForm.php
+++ src/views/WijzigenBedrijfForm.php
@@ -3,53 +3,82 @@
 	<form name="WijzigenFormulier" action="" method="post">
 	<table>
 		<tr>
-		<td><label for="FirstName">Bedrijfsnaam:</label></td><td><input type="text" id="bedrijf_naam" name="Bedrijfsnaam" value="<?php echo $bedrijf_naam; ?>" size="30"/></td>
+			<td><label for="FirstName">Bedrijfsnaam:</label></td>
+			<td><input type="text" id="bedrijf_naam" name="Bedrijfsnaam" value="<?php echo $bedrijf_naam; ?>" /></td>
+			<td><?php echo $NameErr; ?></td>
 		</tr>
 		<tr>
-		<td><label for="Adres">Adres:</label></td><td><input type="text" id="Adres" name="adres" value="<?php echo $adres; ?>"  size="30"/></td>
+			<td><label for="Adres">Adres:</label></td>
+			<td><input type="text" id="Adres" name="adres" value="<?php echo $adres; ?>"  /></td>
+			<td><?php echo $AdresErr; ?></td>
 		</tr>
 		<tr>
-		<td><label for="Postcode">Postcode:</label></td><td><input type="text" id="Postcode" name="postcode" value="<?php echo $postcode; ?>"  size="30"/></td>
+			<td><label for="Postcode">Postcode:</label></td>
+			<td><input type="text" id="Postcode" name="postcode" value="<?php echo $postcode; ?>"  /></td>
+			<td><?php echo $ZipErr; ?></td>
 		</tr>
 		<tr>		
-		<td><label for="Plaats">Plaats:</label></td><td><input type="text" id="Plaats" name="plaats" value="<?php echo $plaats; ?>"  size="30"/></td>
+			<td><label for="Plaats">Plaats:</label></td>
+			<td><input type="text" id="Plaats" name="plaats" value="<?php echo $plaats; ?>"  /></td>
+			<td><?php echo $CityErr; ?></td>
 		</tr>
 		<tr>		
-		<td><label for="provincies">provincies:</label></td><td><input type="text" id="provincies" name="provincies" value="<?php echo $provincies; ?>"  size="30"/></td>
+			<td><label for="provincies">provincies:</label></td>
+			<td><input type="text" id="provincies" name="provincies" value="<?php echo $provincies; ?>"  /></td>
 		</tr>
 		<tr>		
-		<td><label for="Telefoon">Telefoon:</label></td><td><input type="text" id="Telefoon" name="telefoon" value="<?php echo $telefoon; ?>"  size="30"/></td>
+			<td><label for="Telefoon">Telefoon:</label></td>
+			<td><input type="text" id="Telefoon" name="telefoon" value="<?php echo $telefoon; ?>"  /></td>
+			<td><?php echo $TelErr; ?></td>
 		</tr>
 		<tr>		
-		<td><label for="Fax">Fax:</label></td><td><input type="text" id="Fax" name="fax" value="<?php echo $fax; ?>"  size="30"/></td>
+			<td><label for="Fax">Fax:</label></td>
+			<td><input type="text" id="Fax" name="fax" value="<?php echo $fax; ?>"  /></td>
+			<td><?php echo $FaxErr; ?></td>
 		</tr>
 		<tr>		
-		<td><label for="specialiteit">Specialiteit:</label></td><td><input type="text" id="specialiteit" name="specialiteit" value="<?php echo $specialiteit; ?>"  size="30"/></td>
+			<td><label for="specialiteit">Specialiteit:</label></td>
+			<td><input type="text" id="specialiteit" name="specialiteit" value="<?php echo $specialiteit; ?>"  /></td>
 		</tr>
 		<tr>		
-		<td><label for="type">Type:</label></td><td><input type="text" id="type" name="type" value="<?php echo $type; ?>"  size="30"/></td>
+			<td><label for="type">Type:</label></td>
+			<td><input type="text" id="type" name="type" value="<?php echo $type; ?>"  /></td>
 		</tr>
 		<tr>		
-		<td><label for="bereik">Bereik:</label></td><td><input type="text" id="bereik" name="bereik" value="<?php echo $bereik; ?>"  size="30"/></td>
+			<td><label for="bereik">Bereik:</label></td>
+			<td><input type="text" id="bereik" name="bereik" value="<?php echo $bereik; ?>"  /></td>
 		</tr>
 		<tr>
-		<td><label for="transportmanager">Transport-manager:</label></td><td><input type="text" id="transportmanager" name="transportmanager" value="<?php echo $transportmanager; ?>"  size="30"/></td>
+			<td><label for="transportmanager">Transport-manager:</label></td>
+			<td><input type="text" id="transport_manager" name="transportmanager" value="<?php echo $transportmanager; ?>"  /></td>
+			<td><?php echo $TransportErr; ?></td>
 		</tr>
 		<tr>
-		<td><label for="aantal">Aantal:</label></td><td><input type="text" id="aantal" name="aantal" value="<?php echo $aantal; ?>"  size="30"/></td><td>
+			<td><label for="aantal">Aantal:</label></td>
+			<td><input type="number" id="aantal" name="aantal" value="<?php echo $aantal; ?>"  /></td>
 		</tr>
 		<tr>
-		<td><label for="rechtsvorm">Rechtsvorm:</label></td><td><input type="text" id="rechtsvorm" name="rechtsvorm" value="<?php echo $rechtsvorm; ?>"  size="30"/></td>
+			<td><label for="rechtsvorm">Rechtsvorm:</label></td>
+			<td><input type="text" id="rechtsvorm" name="rechtsvorm" value="<?php echo $rechtsvorm; ?>"  /></td>
 		</tr>
 		<tr>
-		<td><label for="vergunning">Vergunning:</label></td><td><input type="text" id="vergunning" name="vergunning" value="<?php echo $vergunning; ?>" size="30"/></td><td>
+			<td><label for="vergunning">Vergunning:</label></td>
+			<td><input type="text" id="vergunning" name="vergunning" value="<?php echo $vergunning; ?>" /></td>
 		</tr>
 		<tr>
-		<td><label for="geldigtot">Geldig tot:</label></td><td><input type="text" id="geldigtot" name="geldigtot" value="<?php echo $geldigtot; ?>" size="30"/></td>
+			<td><label for="geldigtot">Geldig tot:</label></td>
+			<td><input type="text" id="geldigtot" name="geldigtot" value="<?php echo $geldigtot; ?>" /></td>
+			<td><?php echo $GeldigErr; ?></td>
 		</tr>
 		<tr>
-		<td><label for="Email">Bedrijfs E-mail:</label></td><td><input type="text" id="bedrijf_email" name="bedrijf_email" value="<?php echo $bedrijf_email ?>" size="30"/></td>
-		<tr>		
+			<td><label for="Email">Bedrijfs E-mail:</label></td>
+			<td><input type="text" id="bedrijf_email" name="bedrijf_email" value="<?php echo $bedrijfs_email ?>" /></td>
+			<td><?php echo $MailErr; ?></td>
+		</tr>
+		<tr>
+			<td><label for="beschrijving">Beschrijving:</label></td>
+			<td><textarea id="beschrijving" name="beschrijving" value="<?php echo $beschijving; ?>" rows="4" cols="50" /></td>
+		<tr>			
 		<td><input type="submit" name="Wijzigenbedrijf" value="Wijzigen!" /></td>
 		</tr>
 	</table>
