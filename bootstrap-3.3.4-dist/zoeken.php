027467bc31f2479376c17a0e663be0786b0cf735
Stixs <timwestland.94@gmail.com>
2015-06-26 12:49:09 +0200

    zoeken

--- bootstrap-3.3.4-dist/zoeken.php
+++ bootstrap-3.3.4-dist/zoeken.php
@@ -7,23 +7,9 @@ $pdo = ConnectDB();
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+<?php
+	include('head.php');
+?>
   <body>
 	<div class="container">
 		<div class="row box-shadow">
@@ -73,14 +59,8 @@ $pdo = ConnectDB();
 			</div>
 			<?php
 			}
+				require('./controllers/zoeken.php');
 			?>
-			<div class="row">
-				<div class="col-xs-12">
-				<?php
-					require('./controllers/zoeken.php');
-				?>
-				</div>
-			</div>
 		</div>
 	</div>
 
