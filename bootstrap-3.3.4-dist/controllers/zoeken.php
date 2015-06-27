5ac0f53d40c840ad1e7a66e498ba210f7403e010
Stixs <timwestland.94@gmail.com>
2015-06-27 12:04:07 +0200

    Revert "zoeken"
    This reverts commit 027467bc31f2479376c17a0e663be0786b0cf735.

--- bootstrap-3.3.4-dist/controllers/zoeken.php
+++ bootstrap-3.3.4-dist/controllers/zoeken.php
@@ -1,211 +1,135 @@
 <?php
 if(isset($_POST['Zoek']))
 {
-	$trefwoord = NULL;
-	if(!empty($_POST['specialiteit'])){$trefwoord.=''.$_POST['specialiteit'];}
-	if(!empty($_POST['type'])){$trefwoord.=' '.$_POST['type'];}
-	if(!empty($_POST['bereik'])){$trefwoord.=' '.$_POST['bereik'];}
-	if(!empty($_POST['trefwoord'])){$trefwoord.=' '.$_POST['trefwoord'];}
-	
-	//var_dump($trefwoord);
-	
-	$specialiteit = $_POST['specialiteit'];
-	$type = $_POST['type'];
-	$bereik = $_POST['bereik'];
+$trefwoord = $_POST['trefwoord'];
+$specialiteit = $_POST['specialiteit'];
+$type = $_POST['type'];
+$bereik = $_POST['bereik'];
 }
 ?>
-<form id="opnaam" method="post">
-	<div class="row zoeken">
-		<div class="col-xs-7">
-			<select class="form-control search-select" id="sel1" name="specialiteit">
-				<?php
-				
-				$sth = $pdo->prepare('select distinct specialiteit from bedrijfgegevens');
-				$sth->execute();
-				
-				echo '<option value="" selected style="display:none;">Specialiteit</option>';
-				while($row = $sth->fetch())
+
+<div id="zoeken">
+	<form id="opnaam" method="post">
+		<br><br>Zoeken<br>
+		
+		<label for="sel1">Select list:</label>
+		<select class="form-control" id="sel1" name="specialiteit">
+			<?php
+			$specialiteit = NULL;
+			$sth = $pdo->prepare('select distinct specialiteit from bedrijfgegevens');
+			$sth->execute();
+			echo '<option value="" selected style="display:none;">Specialiteit</option>';
+			while($row = $sth->fetch())
+			{
+				if ($row['specialiteit'] == $specialiteit)
 				{
-					if (!isset($specialiteit) or $row['specialiteit'] != $specialiteit)
-					{
-					echo '<option value="'.$row['specialiteit'].'">'.$row['specialiteit'].'</option>';
-					}
-					elseif ($row['specialiteit'] == $specialiteit)
-					{
-					echo '<option value="'.$row['specialiteit'].'" selected>'.$row['specialiteit'].'</option>';	
-					}
+				echo '<option value="'.$row['specialiteit'].'" selected>'.$row['specialiteit'].'</option>';
 				}
-				?>		
-			</select>
-			
-			<select class="form-control search-select" id="sel1" name="type">
-				<?php
-				$sth = $pdo->prepare('select distinct type from bedrijfgegevens');
-				$sth->execute();
-				
-				echo '<option value="" selected style="display:none;">Type</option>';
-				while($row = $sth->fetch())
+				else
 				{
-					if (!isset($type) or $row['type'] != $type)
-					{
-					echo '<option value="'.$row['type'].'">'.$row['type'].'</option>';
-					}
-					elseif ($row['type'] == $type)
-					{
-					echo '<option value="'.$row['type'].'" selected>'.$row['type'].'</option>';	
-					}
+				echo '<option value="'.$row['specialiteit'].'">'.$row['specialiteit'].'</option>';	
 				}
-				?>		
-			</select>
-			
-			<select class="form-control search-select" id="sel1" name="bereik">
-				<?php
-				$sth = $pdo->prepare('select distinct bereik from bedrijfgegevens');
-				$sth->execute();
-				
-				echo '<option value="" selected style="display:none;">Bereik</option>';
-				while($row = $sth->fetch())
+			}
+			?>		
+		</select>
+		
+		<label for="sel1">Select list:</label>
+		<select class="form-control" id="sel1" name="type">
+			<?php
+			$type = NULL;
+			$sth = $pdo->prepare('select distinct type from bedrijfgegevens');
+			$sth->execute();
+			echo '<option value="Type" selected style="display:none;">Type</option>';
+			while($row = $sth->fetch())
+			{
+				if ($row['type'] == $type)
 				{
-					if (!isset($bereik) or $row['bereik'] != $bereik)
-					{
-					echo '<option value="'.$row['bereik'].'">'.$row['bereik'].'</option>';
-					}
-					elseif ($row['bereik'] == $bereik)
-					{
-					echo '<option value="'.$row['bereik'].'" selected>'.$row['bereik'].'</option>';	
-					}
+				echo '<option value="'.$row['type'].'" selected>'.$row['type'].'</option>';
 				}
-				?>		
-			</select>
-		</div>
-		<div class="col-xs-5">
-			<input type="text" name="trefwoord" placeholder="Trefwoord" autofocus size="20">
-			<input type="submit" name="Zoek" value="Zoek"/>
-			 <a href="zoeken.php">Reset</a><br><br>
-		</div>
-	</div>
-</form>
+				else
+				{
+				echo '<option value="'.$row['type'].'">'.$row['type'].'</option>';	
+				}
+			}
+			?>		
+		</select>
+		
+		
+		<label for="sel1">Select list:</label>
+		<select class="form-control" id="sel1" name="bereik">
+			<?php
+			$bereik = NULL;
+			$sth = $pdo->prepare('select distinct bereik from bedrijfgegevens');
+			$sth->execute();
+			echo '<option value="" selected style="display:none;">bereik</option>';
+			while($row = $sth->fetch())
+			{
+				if ($row['bereik'] == $bereik)
+				{
+				echo '<option value="'.$row['bereik'].'" selected >'.$row['bereik'].'</option>';
+				}
+				else
+				{
+				echo '<option value="'.$row['bereik'].'">'.$row['bereik'].'</option>';	
+				}
+			}
+			?>		
+		</select>
+		
+		<input type="text" name="trefwoord" placeholder="Trefwoord" autofocus size="20">
+		<input type="submit" name="Zoek" value="Zoek"/>
+	</form>
 <?php
 	
 if(isset($_POST['Zoek']))
 {
 	
 	//De zoek query
-	$search = NULL;
 	
-	
-	if(!empty($trefwoord))
-		{
-		$loop = 0;
-		$trefwoorden = (explode(" ",$trefwoord));
-		unset($trefwoorden['0']);
-	//	var_dump($trefwoorden);
-		
-		foreach ($trefwoorden as $value)
-			{
-				$search.= '+'.$value.' ';
-				
-			}
-		}
-	
-	//echo $search;
-	
-	if(!empty($search))
-	{
-	$sth = $pdo->prepare('SELECT * FROM bedrijfgegevens WHERE MATCH (bedrijfsnaam, beschrijving, postcode, plaats, provincie, telefoon, fax, transport_manager, rechtsvorm,vergunning, geldig_tot, bedrijfs_email, specialiteit, type, bereik) AGAINST ("'.$search.'" IN BOOLEAN MODE)');
-	}
-	else
-	{
-	$sth = $pdo->prepare('SELECT * FROM bedrijfgegevens');
-	}
+	$sth = $pdo->prepare('select * from bedrijfgegevens where 
+	(bedrijfsnaam like "%'.$trefwoord.'%" or 
+	beschrijving like "%'.$trefwoord.'%" or 
+	adres like "%'.$trefwoord.'%" or 
+	postcode like "%'.$trefwoord.'%" or 
+	plaats like "%'.$trefwoord.'%" or 
+	provincie like "%'.$trefwoord.'%" or 
+	telefoon like "%'.$trefwoord.'%" or 
+	fax like "%'.$trefwoord.'%" or 
+	transport_manager like "%'.$trefwoord.'%" or 
+	aantal like "%'.$trefwoord.'%" or 
+	rechtsvorm like "%'.$trefwoord.'%" or 
+	vergunning like "%'.$trefwoord.'%" or 
+	geldig_tot like "%'.$trefwoord.'%" or 
+	bedrijfs_email like "%'.$trefwoord.'%")	and 
+	(specialiteit like "%'.$specialiteit.'%" and 
+	type like "%'.$type.'%" and 
+	bereik like "%'.$bereik.'%")');
 	$sth->execute();
-	
 	
 	while($row = $sth->fetch())
 	{
-		echo '<div class="row search-result">';
-		echo '<div class="col-xs-12">';
-			while($row = $sth->fetch())
-			{
-				if($row['premium'] == 'ja')
-				{
-				?>
-					<div class="search-container">
-						<div class="search-image">
-						
-							<span class="glyphicon glyphicon-search premium"></span>
-							<img src="images/truck.jpg">
-						</div>
-						<div class="search-naam">
-							<?php echo $row['bedrijfsnaam']; ?>
-						</div>
-					</div>
-				<?php
-				}
-				else
-				{
-					$link = str_replace(" ", "-", $row['bedrijfsnaam']);
-				?>
-					<div class="search-container">
-						<div class="search-image">
-							<span class="glyphicon glyphicon-search no-premium"></span>
-							<img src="images/truck.jpg">
-						</div>
-						<div class="search-naam">
-							<?php echo $row['bedrijfsnaam']; ?>
-						</div>
-					</div>
-				<?php
-				}
-				 
-			}
-		echo '</div>';
-	echo '</div>';
+		echo $row['bedrijfsnaam'].'<br>';
 	}
 	
 }
 else
 {
-	
-	$sth = $pdo->prepare('select * from bedrijfgegevens order by premium');
+	$sth = $pdo->prepare('select * from bedrijfgegevens');
 	$sth->execute();
-	echo '<div class="row search-result">';
-		echo '<div class="col-xs-12">';
-			while($row = $sth->fetch())
-			{
-				if($row['premium'] == 'ja')
-				{
-				?>
-					<div class="search-container">
-						<div class="search-image">
-						
-							<span class="glyphicon glyphicon-search premium"></span>
-							<img src="images/truck.jpg">
-						</div>
-						<div class="search-naam">
-							<?php echo $row['bedrijfsnaam']; ?>
-						</div>
-					</div>
-				<?php
-				}
-				else
-				{
-					$link = str_replace(" ", "-", $row['bedrijfsnaam']);
-				?>
-					<div class="search-container">
-						<div class="search-image">
-							<span class="glyphicon glyphicon-search no-premium"></span>
-							<img src="images/truck.jpg">
-						</div>
-						<div class="search-naam">
-							<?php echo $row['bedrijfsnaam']; ?>
-						</div>
-					</div>
-				<?php
-				}
-				 
-			}
-		echo '</div>';
-	echo '</div>';
+	
+	while($row = $sth->fetch())
+	{
+		?>
+		<div class="search-container">
+			<div class="search-image">
+				
+			</div>
+			<div class="search-naam">
+				<?php echo $row['bedrijfsnaam']; ?>
+			</div>
+		</div>
+		<?php
+	}
 }
 ?>
+</div>
