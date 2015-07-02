5c999a697f616bb77b4e7338850ba6f4a8a91f5c
jensvanlankveld <jensvanlankveld@Live.nl>
2015-07-02 15:15:10 +0200

    inloggen

--- src/views/InloggenForm.php
+++ src/views/InloggenForm.php
@@ -1,9 +1,18 @@
-inloggen<br />
-<br />
-<form method="post" action="" name="inloggen">
-	Gebruikersnaam.<br />
-	<input type="text" name="Username" /><br />
-	Wachtwoord.<br />
-	<input type="password" name="Password" /><br />
-	<input type="submit" name="Inloggen" value="inloggen" />
-</form>
+
+
+<div class="row">
+	<div class="col-xs-4 col-xs-offset-4">
+		<h1>inloggen</h1>
+		<form method="post" action="" name="inloggen">
+			<div class="form-group">
+				<label for="gebruikersnaam">Gebruikersnaam.</label>
+				<input type="text" class="form-control" name="Username" />
+			</div>
+			<div class= "form-group">
+				<label for="wachtwoord">Wachtwoord.</label>
+				<input type="password" class="form-control" name="Password" />
+			</div>
+			<input type="submit" class="btn btn-default" name="Inloggen" value="inloggen" />
+		</form>
+	</div>
+</div>
