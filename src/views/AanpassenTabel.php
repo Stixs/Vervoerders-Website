9b25e9ca2d4a0bdf3a66339b5643a24743ff0c2b
Stixs <timwestland.94@gmail.com>
2015-07-02 13:56:04 +0200

    links repareren

--- src/views/AanpassenTabel.php
+++ src/views/AanpassenTabel.php
@@ -21,8 +21,8 @@ $sth->execute();
 				<td><?php echo $row['adres']; ?></td>
 				<td><?php echo $row['plaats']; ?></td>
 				<td><?php echo $row['telefoon']; ?></td>
-				<td><a class="btn btn-default" role="button" href="./aanpassen.php?action=edit&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Aanpassen</a></td>
-				<td><a class="btn btn-default" role="button" href="./aanpassen.php?action=del&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Verwijderen</a></td>
+				<td><a class="btn btn-default" role="button" href="./index.php?paginanr=4&action=edit&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Aanpassen</a></td>
+				<td><a class="btn btn-default" role="button" href="./index.php?paginanr=4&action=del&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Verwijderen</a></td>
 			</tr>
 		<?php 
 		} 
