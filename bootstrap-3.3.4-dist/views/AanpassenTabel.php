8ae065364e70014a229c285c953969a896ea84b4
jensvanlankveld <jensvanlankveld@Live.nl>
2015-07-02 10:09:10 +0200

    Revert "Update aanpassen"
    This reverts commit 87697e77203de980dbbb928e75d412e9cb91ccbb.

--- bootstrap-3.3.4-dist/views/AanpassenTabel.php
+++ bootstrap-3.3.4-dist/views/AanpassenTabel.php
@@ -2,40 +2,26 @@
 $sth = $pdo->prepare('select * form bedrijfgegevens');
 $row = $sth->fetch();
  ?>
-<div class="row">
-	<div class="col-xs-3">
-		Bedrijfs id
-	</div>
-	<div class="col-xs-3">
-		Bedrijfs naam
-	</div>
-	<div class="col-xs-3">
-		Aanpassen
-	</div>
-	<div class="col-xs-3">
-		Verwijderen
-	</div>
-</div>
-	<?php
+<div class="col-xs-12">
+	<table class="table table-bordered">
+		<tr>
+			<th>Bedrijfs id</th>
+			<th>Bedrijfs naam</th>
+			<th>Aanpassen</th>
+			<th>Verwijderen</th>
+		</tr>
+		<?php
 		while($row = $sth->fetch()) 
 		{ 
 		?>
-		<div class="row">
-			<div class="col-xs-3">
-				<?php echo $row['bedrijfs_id'];?>
-			</div>
-			<div class="col-xs-3">
-				<?php echo $row['bedrijfsnaam']; ?>
-			</div>
-			<div class="col-xs-3">
-				<a class="btn btn-default" role="button" href="./aanpassen.php?action=edit&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Aanpassen</a>
-			</div>
-			<div class="col-xs-3">
-				<a class="btn btn-default" role="button" href="./aanpassen.php?action=del&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Verwijderen</a>
-			</div>
-		</div>
+			<tr>
+				<td><?php echo $row['bedrijfs_id'];?></td>
+				<td><?php echo $row['bedrijfsnaam']; ?></td>
+				<td><a class="btn btn-default" role="button" href="./aanpassen.php?action=edit&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Aanpassen</a></td>
+				<td><a class="btn btn-default" role="button" href="./aanpassen.php?action=del&bedrijfs_id=<?php echo $row['bedrijfs_id']; ?>">Verwijderen</a></td>
+			</tr>
 		<?php 
 		} 
 		?>
-
+	</table>
 </div>
